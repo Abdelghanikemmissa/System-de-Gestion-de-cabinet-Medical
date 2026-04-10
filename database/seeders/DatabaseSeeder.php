01446e70aaede6866c750683063dc78f01245aa0
Abdelghanikemmissa <abdokemmissa@gmail.com>
2026-04-10 16:13:15 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
         $userSecretaire = User::create([
             'nom' => 'Assistante',
             'prenom' => 'Julie',
+            'date_naissance' => '1990-01-01',
             'email' => 'secretaire@test.com',
             'password' => Hash::make('password'),
             'cni' => 'SEC456',
@@ -37,12 +38,12 @@ class DatabaseSeeder extends Seeder
 
         // 3. CRÉATION DE 6 PATIENTS
         $patientsData = [
-            ['nom' => 'Dupont', 'prenom' => 'Jean', 'email' => 'jean@test.com', 'cni' => 'BE200200'],
-            ['nom' => 'Martin', 'prenom' => 'Alice', 'email' => 'alice@test.com', 'cni' => 'AA300300'],
-            ['nom' => 'Lefebvre', 'prenom' => 'Thomas', 'email' => 'thomas@test.com', 'cni' => 'CC400400'],
-            ['nom' => 'Moreau', 'prenom' => 'Sophie', 'email' => 'sophie@test.com', 'cni' => 'DD505505'],
-            ['nom' => 'Petit', 'prenom' => 'Lucas', 'email' => 'lucas@test.com', 'cni' => 'EE606606'],
-            ['nom' => 'Rousseau', 'prenom' => 'Emma', 'email' => 'emma@test.com', 'cni' => 'FF707707'],
+            ['nom' => 'Dupont', 'prenom' => 'Jean', 'date_naissance' => '1985-05-15', 'email' => 'jean@test.com', 'cni' => 'BE200200'],
+            ['nom' => 'Martin', 'prenom' => 'Alice', 'date_naissance' => '1992-08-20', 'email' => 'alice@test.com', 'cni' => 'AA300300'],
+            ['nom' => 'Lefebvre', 'prenom' => 'Thomas', 'date_naissance' => '1988-12-10', 'email' => 'thomas@test.com', 'cni' => 'CC400400'],
+            ['nom' => 'Moreau', 'prenom' => 'Sophie', 'date_naissance' => '1995-03-25', 'email' => 'sophie@test.com', 'cni' => 'DD505505'],
+            ['nom' => 'Petit', 'prenom' => 'Lucas', 'date_naissance' => '1990-11-30', 'email' => 'lucas@test.com', 'cni' => 'EE606606'],
+            ['nom' => 'Rousseau', 'prenom' => 'Emma', 'date_naissance' => '1993-07-18', 'email' => 'rousseau@gmail.com']
         ];
 
         foreach ($patientsData as $data) {
@@ -52,6 +53,7 @@ class DatabaseSeeder extends Seeder
                 'email' => $data['email'],
                 'password' => Hash::make('password'),
                 'cni' => $data['cni'],
+                'date_naissance' => $data['date_naissance'],
                 'role' => 'patient',
             ]);
 
